4192fa41df8695ef86c6328697b494bafe875e2d
Rachael Salter <rasalter@gmail.com>
2015-07-23 00:23:34 +0000

    Added address and descrip validations

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
-	validates :name, :presence => true
+	validates :name, :presence => true, :length => { :minimum => 3, :maximum => 50}
+	validates :address, :presence => true, :length => { :minimum => 15, :maximum => 100}
+	validates :description, :presence => true, :length => { :minimum => 15, :maximum => 200}
 end
