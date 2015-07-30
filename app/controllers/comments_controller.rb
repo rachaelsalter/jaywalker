4d4a3ba07ea20f3a3ce4fe2587a5225c17bdbede
Rachael Salter <rasalter@gmail.com>
2015-07-30 13:06:39 +0000

    update to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-	before_action :authenticate_user!, :only => [:create]
+	before_action :authenticate_user!
 	def create  
 		@place = Place.find(params[:place_id])
 		@place.comments.create(comment_params.merge(:user => current_user))
