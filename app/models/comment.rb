a97c50a2fa33fc20419a1e8b8c8b23c835696eca
Rachael Salter <rasalter@gmail.com>
2015-08-01 22:37:27 +0000

    Add dropdown box for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :place
-end
+
+	RATINGS = {
+		'one star' => '1_star',
+		'two stars' => 'two_stars',
+		'three stars' => 'three_stars',
+		'four stars' => 'four_stars',
+		'five stars' => 'five_stars'
+	}
+
+	def humanized_rating
+		RATINGS.invert[self.rating]
+	end 
+end 
