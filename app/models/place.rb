42a9bea1e7695414098ff46e6710afd48a169251
Rachael Salter <rasalter@gmail.com>
2015-08-02 03:14:40 +0000

    step one of email notifications of place comments

--- app/models/place.rb
+++ app/models/place.rb
@@ -6,4 +6,10 @@ class Place < ActiveRecord::Base
 	validates :name, :presence => true, :length => { :minimum => 3, :maximum => 50}
 	validates :address, :presence => true, :length => { :minimum => 15, :maximum => 100}
 	validates :description, :presence => true, :length => { :minimum => 15, :maximum => 200}
+
+	def last_comment
+		self.comments.order("id ASC").last
+	end
 end
+
+
