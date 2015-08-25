0c137ac2a1a0ea930d6e3eb2835977627c1182b8
Rachael Salter <rasalter@gmail.com>
2015-08-25 01:09:56 +0000

    Controller test - user create comment, successful

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "user create a comment" do
+		user = FactoryGirl.create(:user)
+		sign_in user
+			place = FactoryGirl.create(:place)
+
+			assert_difference 'Comment.count' do 
+				post :create, :place_id => place.id, :comment => {
+					:message => 'I love this place',
+					:rating => 'five_stars',
+				}
+		end
+
+			assert_redirected_to place_path(place)
+			assert_equal 1, place.comments.count   
+	end
 end
